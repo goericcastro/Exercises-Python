d94920d71b0e41eddc67495619cdf18b95ea35d7
Eric Castro <87159078+goericcastro@users.noreply.github.com>
2022-11-15 15:59:06 -0300

    Desafio Dio
    distancia de entre Palatir Saruman e Saurom

--- Palatir_icm_listas.py
+++ Palatir_icm_listas.py
@@ -5,4 +5,5 @@ diametro1 = int(entrada[1])
 diametro2 = int(entrada[2])
 
 if (0 < distancia < 10000) and (0<diametro1<100) and (0<diametro2<100):
-    print ("icm:",distancia/(diametro1+diametro2))
+    icm= distancia / (diametro1+diametro2)
+    print (f"{icm:.2f}")
